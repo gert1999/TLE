f825128a1ed5e940767aaeda9fb4ea7e9c8e6109
Esolutions-IT <43686864+Esolutions-IT@users.noreply.github.com>
2020-11-17 13:26:30 +0100

    Grafiek
    Grafiek basis

--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -18,11 +18,17 @@
     while ($row = mysqli_fetch_array($result)) {
 
         $data1 = $data1 . '"'. $row['score'].'",';
-
-
     }
 
+    $row2 = mysqli_num_rows($result);
+
     $data1 = trim($data1,",");
+
+//    $sql2 = "SELECT COUNT(*) FROM feelings WHERE `student_id` = $id";
+//    $result2 = mysqli_query($mysqli, $sql2);
+//
+//    $row3 = mysqli_num_rows($result2);
+//
 
 ?>
 
@@ -30,8 +36,25 @@
 <link rel="stylesheet" href="{{ public_path('css/font-awesome/css/font-awesome.min.css') }}" />
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.bundle.min.js"></script>
+<script type="text/javascript" src="https://rawgit.com/nnnick/Chart.js/v1.0.2/Chart.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
 
 <style>
+    .chartWrapper {/* w w w. j a v  a2 s.co  m*/
+        position: relative;
+    }
+    .chartWrapper > canvas {
+        position: absolute;
+        left: 0;
+        top: 0;
+        pointer-events:none;
+    }
+    .chartAreaWrapper {
+        width: 100%;
+        overflow-x: scroll;
+    }
+
     .btn {
         background-color: #f5d142;
         border: none;
@@ -42,6 +65,11 @@
     }
     .btn:hover {
         background-color: RoyalBlue;
+    }
+
+    .mydataChart{
+        width:0px;
+        height:300px;
     }
     /*body{*/
     /*    font-family: Arial;*/
@@ -96,30 +124,84 @@
     </div>
 
     <div class="container">
-        <canvas id="chart" style="width: 100%; height: 65vh; background: #222; border: 1px solid #555652; margin-top: 10px;"></canvas>
+{{--        <canvas id="chart" style="width: 100%; height: 65vh; background: #222; border: 1px solid #555652; margin-top: 10px;"></canvas>--}}
+
+        <div class="chartWrapper">
+            <div class="chartAreaWrapper">
+                <canvas id="myChart" class="mydataChart"></canvas>
+            </div>
+            <canvas id="myChartAxis" height="300" width="0"></canvas>
+        </div>
+    </div>
 
         <script>
-            var ctx = document.getElementById("chart").getContext('2d');
-            var myChart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: [1,2,3,4,5,6,7,8,9],
-                    datasets:
-                        [{
-                            label: 'Hoi',
-                            data: [<?php echo $data1 ?>],
-                            backgroundColor: 'transparent',
-                            borderColor:'rgba(255,99,132)',
-                            borderWidth: 5,
-                        }]
-                },
+            {{--var ctx = document.getElementById("chart").getContext('2d');--}}
+            {{--var myChart = new Chart(ctx, {--}}
+            {{--    type: 'line',--}}
+            {{--    data: {--}}
+            {{--        labels: [1,2,3,4,5,6,7,8,9,10],--}}
+            {{--        datasets:--}}
+            {{--            [{--}}
+            {{--                label: 'Emoties',--}}
+            {{--                data: [<?php echo $data1 ?>],--}}
+            {{--                backgroundColor: 'transparent',--}}
+            {{--                borderColor:'rgba(255,99,132)',--}}
+            {{--                borderWidth: 5,--}}
+            {{--            }]--}}
+            {{--    },--}}
 
-                options: {
-                    scales: {scales:{yAxes: [{beginAtZero: false}], xAxes: [{autoskip: true, maxTicketsLimit: 20}]}},
-                    tooltips:{mode: 'index'},
-                    legend:{display: true, position: 'top', labels: {fontColor: 'rgb(255,255,255)', fontSize: 16}}
-                }
-            });
+            {{--    options: {--}}
+            {{--        scales: {scales:{yAxes: [{beginAtZero: false}], xAxes: [{autoskip: true, maxTicketsLimit: 20}]}},--}}
+            {{--        tooltips:{mode: 'index'},--}}
+            {{--        legend:{display: true, position: 'top', labels: {fontColor: 'rgb(255,255,255)', fontSize: 16}}--}}
+            {{--    }--}}
+            {{--});--}}
+
+                var ctx = document.getElementById("myChart").getContext("2d");
+
+            var labelsData = [];
+            for (i = 1; i <= <?php echo $row2 ?>; i++) {
+                // text += cars[i] + "<br>";
+                labelsData.push(i);
+            }
+
+                let data = {
+                    labels: labelsData,
+                    datasets: [
+                        {
+                            label: "My First dataset",
+                            fillColor: "rgba(220,220,220,0.2)",
+                            strokeColor: "rgba(220,220,220,1)",
+                            pointColor: "rgba(220,220,220,1)",
+                            pointStrokeColor: "#fff",
+                            pointHighlightFill: "#fff",
+                            pointHighlightStroke: "rgba(220,220,220,1)",
+                            data: [<?php echo $data1 ?>]
+                        },
+                    ]
+                };
+                // document.getElementsByClassName("mydataChart").style.width = widthData + "px";
+                $(".mydataChart").css("width", data.labels.length * 150 + "px");
+
+                new Chart(ctx).Line(data, {
+                    onAnimationComplete: function () {
+                        var sourceCanvas = this.chart.ctx.canvas;
+                        var copyWidth = this.scale.xScalePaddingLeft - 5;
+                        var copyHeight = this.scale.endPoint + 5;
+                        var targetCtx = document.getElementById("myChartAxis").getContext("2d");
+                        targetCtx.canvas.width = copyWidth;
+                        targetCtx.drawImage(sourceCanvas, 0, 0, copyWidth, copyHeight, 0, 0, copyWidth, copyHeight);
+                    }
+                });
+
+                // if(data.labels.length > 10){
+                //     // document.getElementById("myChart").style.width = "300%";
+                // }
+                // $(".chartAreaWrapper").scrollTop(100);
+
+
+
+
         </script>
     </div>
 </x-app-layout>
